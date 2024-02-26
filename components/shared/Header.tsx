9617896aa1a5b877959b4b71adc5843360f2db66
Vladimir Dyadenko <Stilmagroup@gmail.com>
2024-02-26 22:36:38 +0200

    addad mongodb

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -2,10 +2,12 @@ import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "../ui/button";
+import NavItems from "./NavItems";
+import MobilNav from "./MobilNav";
 
 const Header = () => {
   return (
-    <header className="w-full border-b ">
+    <header className="w-full border-b">
       <div className="wrapper flex items-center justify-between">
         <Link href="/" className="w-36">
           <Image
@@ -15,10 +17,18 @@ const Header = () => {
             height={38}
           />
         </Link>
+        <SignedIn>
+          <nav className="md:flex-between hidden w-full max-w-xs">
+            <NavItems />
+          </nav>
+        </SignedIn>
+
         <div className="flex w-32 justify-end gap-3">
           <SignedIn>
             <UserButton afterSignOutUrl="/" />
+            <MobilNav />
           </SignedIn>
+
           <SignedOut>
             <Button asChild className="rounded-full" size="lg">
               <Link href="/sign-in">Login</Link>
